3b78c3c082c3d96b2b308bf2e7ec240f1321d824
Genesis Jesus <genesisdev96@outlook.com>
2016-11-03 16:54:38 -0500

    Add files via upload

---
+++ MetodoLehmer.py
@@ -0,0 +1,30 @@
+#Numeros pseudoAleatorios por el metodo lehmer
+#Genesis Jesus Reyes Osnaya
+
+contador = 1
+xo = int(input("Ingresa la semilla: ")) #Ingresamos la semilla
+c = int(input("Ingresa el numero c: ")) #Ingresamos el numero C
+numero = int(input("Ingresa la cantidad de numeros: ")) #Ingresamos la cantidad
+xoj = xo
+
+if(xo>c):
+    print("")
+    print("Resta: " , xo)
+    while(contador < numero):
+        
+        multiStr = str(xo * c) #Se hace la multiplicacion
+
+        ope = len(multiStr) - len(str(xoj)) #Sacamos los tamanos
+
+        derecha = int(multiStr[ope:])
+        izquierda = int(multiStr[:ope])
+        resultado = derecha - izquierda #Restamos el numero mayor con el menor
+
+
+        xo = resultado
+        print("Resta: " , xo)
+        contador = contador + 1
+
+else:
+    print("")
+    print("La semilla debe ser mas grande que el numero")
